c4fd4186c7b7a0e6f12bfcc960f030172044a876
junu <leej036@newschool.edu>
2024-11-15 23:54:44 -0500

    fixed bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useRef, useEffect } from 'react'
+// import { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
