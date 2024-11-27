e6e5d22efd4dc6da3f212a7d7c5307fbc3042882
junu <leej036@newschool.edu>
2024-11-27 01:17:54 -0500

    fixed again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 // import { useState, useEffect } from 'react'
 
 // import React, { useEffect, useRef } from 'react'
-import React, { Suspense } from 'react'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
 import { ProjectGrid } from "@/components/ui/card"
