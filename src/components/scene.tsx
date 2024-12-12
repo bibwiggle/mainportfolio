c0ccc507d046db61607764113418d4b59e3e676d
junu <leej036@newschool.edu>
2024-12-11 19:03:25 -0500

    fix

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,30 +1,30 @@
 "use client"
 
-import React, { useRef, useMemo } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { OrbitControls, CubeCamera, Environment } from '@react-three/drei'
-import * as THREE from 'three'
+import React, {  } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, CubeCamera } from '@react-three/drei'
+// import * as THREE from 'three'
 
 // Import your project images
-const projectImages = [
-  "/dancheong.png",
-  "/dancheong.png",
-  "/dancheong.png",
-  "/dancheong.png",
-  "/dancheong.png",
-]
+// const projectImages = [
+//   "/dancheong.png",
+//   "/dancheong.png",
+//   "/dancheong.png",
+//   "/dancheong.png",
+//   "/dancheong.png",
+// ]
 
 
 function ReflectiveSphere() {
-  const meshRef = useRef<THREE.Mesh>(null)
-  const cubeRenderTarget = useMemo(() => new THREE.WebGLCubeRenderTarget(256), [])
-  const textures = useLoader(THREE.TextureLoader, projectImages)
+  // const meshRef = useRef<THREE.Mesh>(null)
+  // const cubeRenderTarget = useMemo(() => new THREE.WebGLCubeRenderTarget(256), [])
+  // const textures = useLoader(THREE.TextureLoader, projectImages)
 
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 1
-    }
-  })
+  // useFrame(() => {
+  //   if (meshRef.current) {
+  //     meshRef.current.rotation.y += 1
+  //   }
+  // })
 
   return (
     <CubeCamera frames={Infinity}>
@@ -38,23 +38,23 @@ function ReflectiveSphere() {
   );
 }
 
-function EnvironmentCube() {
-  const textures = useLoader(THREE.TextureLoader, projectImages)
-  const envMap = useMemo(() => {
-    const cubeTexture = new THREE.CubeTexture(textures)
-    cubeTexture.needsUpdate = true
-    return cubeTexture
-  }, [textures])
+// function EnvironmentCube() {
+//   const textures = useLoader(THREE.TextureLoader, projectImages)
+//   const envMap = useMemo(() => {
+//     const cubeTexture = new THREE.CubeTexture(textures)
+//     cubeTexture.needsUpdate = true
+//     return cubeTexture
+//   }, [textures])
 
-  return <Environment files='/HDR_blue_nebulae-1.hdr' background={false} />
-}
+//   return <Environment files='/HDR_blue_nebulae-1.hdr' background={false} />
+// }
 
 export function Scene() {
   return (
     <div style={{ width: '100%', height: '100vh', position: 'relative', pointerEvents: 'none'}}>
       <Canvas camera={{ position: [0, 0, 500], fov: 75 }} gl={{ alpha: true }}>
         <React.Suspense fallback={null}>
-          <EnvironmentCube />
+          {/* <EnvironmentCube /> */}
           <ReflectiveSphere />
           <OrbitControls enableZoom={false} enablePan={true} />
           <ambientLight intensity={0.5} />
