06a576cc4224691ea3a33c9b0741dc14fafcb2d6
junu <leej036@newschool.edu>
2025-01-23 15:39:06 -0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { roboto, montserrat } from "@/fonts";
+import { montserrat } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Junu Lee",
