b78c6de46e6d04c9ff308561a4af24e853e82f7c
Joon Woo Lee <bibw.lab@gmail.com>
2026-04-19 00:36:17 -0400

    x button added

--- src/components/LightboxImage.tsx
+++ src/components/LightboxImage.tsx
@@ -26,6 +26,12 @@ export function LightboxImage({ src, alt = "", className = "", style, imageClass
           className="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4 cursor-zoom-out"
           onClick={() => setOpen(false)}
         >
+          <button
+            className="absolute top-4 right-4 text-white/60 hover:text-white text-3xl leading-none cursor-pointer"
+            onClick={() => setOpen(false)}
+          >
+            ×
+          </button>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src={src} alt={alt} className="max-w-full max-h-full object-contain rounded-lg" />
         </div>
--- src/components/LightboxVideo.tsx
+++ src/components/LightboxVideo.tsx
@@ -26,6 +26,12 @@ export function LightboxVideo({ src, className = "w-full rounded-lg", style, wra
           className="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4 cursor-zoom-out"
           onClick={() => setOpen(false)}
         >
+          <button
+            className="absolute top-4 right-4 text-white/60 hover:text-white text-3xl leading-none cursor-pointer"
+            onClick={() => setOpen(false)}
+          >
+            ×
+          </button>
           <video
             src={src}
             controls
